19a30b966c794c229cf16d1e685d4203f4a1a6c4
Rhys-011003 <rod.rhys20031@gmail.com>
2022-10-06 23:04:04 +0530

    color scheme

--- Scripts/Particles/app.js
+++ Scripts/Particles/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 20,
+        "value": 8,
         "density": {
           "enable": true,
           "value_area": 800
